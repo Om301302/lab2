4bdce24c73da4bd7ac57bde1416c8bd3ce17d188
Om301302 <80860398+Om301302@users.noreply.github.com>
2021-09-07 19:09:24 -0700

    Update displaySummary.java
    added

--- src/displaySummary.java
+++ src/displaySummary.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class displaySummary {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+    
+        System.out.println("Summary");
+        System.out.println("Initial Balance: " + initBal);
+        System.out.println("Total incomes: " + totInc);
+        System.out.println("Total expence: " + totExp);
+        System.out.println("Final balance: " + finalBal);
+        System.out.println("Good Bye!");
+
+
+    }
 
     }
 }
